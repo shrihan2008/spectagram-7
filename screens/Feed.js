6669f68c677dbbedf036218a6cd127e22f3b3bc7
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-01-24 15:05:20 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import PostCard from "../screens/PostCard";
+
+import { FlatList } from "react-native-gesture-handler";
+import PostScreen from '../screens/PostScreen'
+
+let post = require("../temp_post.json");
+
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+   
+  }
+
+
+  renderItem = ({ item: post }) => {
+    return <PostCard post={post} navigation={this.props.navigation.navigate}/>;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+        return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Spectagram</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={post}
+              renderItem={this.renderItem}
+            />
+          </View>
+        </View>
+      );
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+   
+  },
+  cardContainer: {
+    flex: 0.93
+  }
+});
